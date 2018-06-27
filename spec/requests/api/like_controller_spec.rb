1603329b66076a4e1d78350478086586b0c4e039
Ashley Foster <ashley@ashleyfoster.me>
2018-06-27 10:44:21 -0400

    Spec Cleanup

--- spec/requests/api/like_controller_spec.rb
+++ spec/requests/api/like_controller_spec.rb
@@ -1,13 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "LikesController" do
-  describe '#create' do
+describe 'LikesController' do
+  describe 'POST#create' do
     it 'creates a like for a signed in user' do
       user = create(:user)
       post = create(:post, id: 1)
 
       expect{
-        post "/api/posts/#{post.id}/like", headers: {
+        post "/api/posts/#{post.id}/like",
+        headers: {
           'X-User-Email' => user.email,
           'X-User-Token' => user.authentication_token
         }
@@ -24,32 +25,34 @@ describe "LikesController" do
     end
   end
 
-  describe "DELETE #destroy" do
-    context "when user owns like" do
-      it "deletes the like" do
+  describe 'DELETE#destroy' do
+    context 'when user owns like' do
+      it 'deletes the like' do
         user = create(:user)
         post = create(:post, id: 1)
         create(:like, user: user, post: post)
 
         expect{
-          delete "/api/posts/#{post.id}/like", headers: {
+          delete "/api/posts/#{post.id}/like",
+          headers: {
             'X-User-Email' => user.email,
-              'X-User-Token' => user.authentication_token
+            'X-User-Token' => user.authentication_token
           }
         }.to change{Like.count}.by(-1)
         expect(response.status).to eq(204)
       end
     end
 
-    context "when user does not own like" do
-      it "does not delete the like" do
+    context 'when user does not own like' do
+      it 'does not delete the like' do
         owner = create(:user, id: 1)
         user = create(:user, id: 2)
         post = create(:post, id: 1)
         create(:like, user: owner, post: post)
 
         expect{
-          delete "/api/posts/#{post.id}/like", headers: {
+          delete "/api/posts/#{post.id}/like",
+          headers: {
             'X-User-Email' => user.email,
             'X-User-Token' => user.authentication_token
           }
